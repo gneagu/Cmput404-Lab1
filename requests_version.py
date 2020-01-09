4d645136348b2c5a7bb3c3588f6bfdab57501f9b
gneagu <neagu@ualberta.ca>
2020-01-08 20:32:27 -0500

    added final curl to git page

--- requests_version.py
+++ requests_version.py
@@ -1,7 +1,10 @@
 import requests
-
 print(requests.__version__)
 
 
 google = requests.get("https://www.google.com")
 print(google)
+
+
+script = requests.get("https://raw.githubusercontent.com/gneagu/Cmput404-Lab1/m$
+print(script.content)
